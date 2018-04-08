f080ebc7e6106181ae23930de4167e11bec3ba16
Gautham Kumar <heygautham@gmail.com>
2018-04-08 13:36:24 +0530

    Removed interests from team page

--- js/members.js
+++ js/members.js
@@ -10,19 +10,19 @@ $(document).ready(function() {
                 // Populate Fourth Years
                 var members = result.fourthYears;
                 for (var i = 0;i<members.length;i++) {
-                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><h5>Interests</h5><h5>Full Stack Development, Music</h5><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
+                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
                 }
 
                 // Pupulate Third Years
                 var members = result.thirdYears;
                 for (var i = 0;i<members.length;i++) {
-                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><h5>Interests</h5><h5>Full Stack Development, Music</h5><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
+                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
                 }
 
                 // Populate Second Years
                 var members = result.secondYears;
                 for (var i = 0;i<members.length;i++) {
-                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><h5>Interests</h5><h5>Full Stack Development, Music</h5><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
+                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
                 }
             }
         }
