e8faf2d8d468f356b96bf044bb7ae1f8f6f3feba
Gautham Kumar <heygautham@gmail.com>
2018-04-10 17:54:38 +0530

    Members rework with tabs

--- js/members.js
+++ js/members.js
@@ -1,27 +1,67 @@
+var members = {};
+
 $(document).ready(function() {
     $.ajax(
         {
             url: "/data/members.json",
             success: function(result) {
-
-                // Populate Fourth Years
-                var members = result.fourthYears;
-                for (var i = 0;i<members.length;i++) {
-                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
-                }
-
-                // Pupulate Third Years
-                var members = result.thirdYears;
-                for (var i = 0;i<members.length;i++) {
-                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
-                }
-
-                // Populate Second Years
-                var members = result.secondYears;
-                for (var i = 0;i<members.length;i++) {
-                    $('#allMembersContainer').append('<div class="member-container"><div class="basic-details"><img src="images/member.png" class="member-image"><h3 class="member-name">'+members[i].name+'</h3></div><div class="inner-details"><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span><a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github textLogo"></i></a></span></div></div></div>');
-                }
+                loadFourthYears(result.fourthYears);
+                loadThirdYears(result.thirdYears);
+                loadSecondYears(result.secondYears);
+                $('#allMembersContainer').html(members['fourthYears']+members['thirdYears']+members['secondYears']);
             }
         }
     );
 })
+
+function getHTMLString(person) {
+    return `<div class="member-container"><div class="basic-details"><img src="images/member.png" 
+    class="member-image"> <h3 class="member-name">${person.name}</h3></div><div class="inner-details 
+    ${person.desc_1}"><div class="social-info"><span><a href="https://www.linkedin.com/in/gautham-kumar" 
+    target="_blank"><i id="linkedinLogo" class="fab fa-linkedin-in textLogo"></i></a></span><span>
+    <a href="https://www.github.com/gauthamk97" target="_blank"><i id="githubLogo" class="fab fa-github 
+    textLogo"></i></a></span></div></div></div>`;
+}
+
+function loadFourthYears(fourthYears) {
+    // Populate Fourth Years
+    members['fourthYears'] = fourthYears.map(fourthYear => getHTMLString(fourthYear)).join();
+}
+
+function loadThirdYears(thirdYears) {
+    // Populate Third Years
+    members['thirdYears'] = thirdYears.map(thirdYear => getHTMLString(thirdYear)).join();
+}
+
+function loadSecondYears(secondYears) {
+    // Populate Second Years
+    members['secondYears'] = secondYears.map(secondYear => getHTMLString(secondYear)).join();
+}
+
+function handleActiveTab(selectedTab) {
+    var buttons = $('#batchButtons').children();
+    for (button of buttons) {
+        $(button).removeClass("active-batch");
+    }
+    $(selectedTab).addClass("active-batch");
+}
+
+function clickAll() {
+    $('#allMembersContainer').html(members['fourthYears']+members['thirdYears']+members['secondYears']);
+    handleActiveTab(event.currentTarget);
+}
+
+function clickFourthYears() {
+    $('#allMembersContainer').html(members['fourthYears']);
+    handleActiveTab(event.currentTarget);
+}
+
+function clickThirdYears() {
+    $('#allMembersContainer').html(members['thirdYears']);
+    handleActiveTab(event.currentTarget);
+}
+
+function clickSecondYears() {
+    $('#allMembersContainer').html(members['secondYears']);
+    handleActiveTab(event.currentTarget);
+}
