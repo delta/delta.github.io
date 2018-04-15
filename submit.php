358a0d91b20e84be05bc87fff3dbb4a29c5b9e3b
venkat24 <venkatramansrikanth@outlook.com>
2018-04-15 22:34:28 +0530

    Add inductions registration backend

---
+++ submit.php
@@ -0,0 +1,45 @@
+<?php
+
+require("config.php");
+
+$name = $_POST['name'];
+$rollno = $_POST["rollno"];
+$email = $_POST["email"];
+$phoneno = $_POST["phoneno"];
+$gender = $_POST["gender"];
+$github = $_POST["github"];
+$webdev = $_POST["webdev"];
+$appdev = $_POST["appdev"];
+$sysad = $_POST["sysad"];
+$open = $_POST["open"];
+$interests = $_POST["interests"];
+
+if (   $_POST['name'] == ""
+    || $_POST["rollno"] == ""
+    || $_POST["email"] == ""
+    || $_POST["phoneno"] == ""
+    || $_POST["gender"] == ""
+    || $_POST["github"] == ""
+    || $_POST["webdev"] == ""
+    || $_POST["appdev"] == ""
+    || $_POST["sysad"] == ""
+    || $_POST["open"] == ""
+) {
+    echo "Failure";
+    return;
+}
+
+$sql = "INSERT INTO registrations"
+    ." (name, rollno, phoneno, gender, email, github, webdev, appdev, sysad, open, interests)"
+    ." values (?,?,?,?,?,?,?,?,?,?,?)";
+
+$statement = $conn->prepare($sql);
+
+$statement->bind_param("sssssssssss",
+    $name, $rollno, $phoneno, $gender, $email, $github, $webdev, $appdev, $sysad, $open, $interests);
+
+$statement->execute();
+
+echo "Success";
+
+?>
